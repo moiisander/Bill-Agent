353b7c8f768624aa3de372f568ab34154d9caf67
Ivar <ivar.moisander@advania.se>
2025-08-22 18:49:47 +0200

    setup shadcn

--- client/src/App.tsx
+++ client/src/App.tsx
@@ -1,5 +1,4 @@
 import { useState } from "react";
-import "./App.css";
 import { QueryClient, QueryClientProvider } from "@tanstack/react-query";
 import { trpc } from "./utils/trpc";
 import { httpBatchLink } from "@trpc/client";
